772de5253f428bd81ad9b9dd206de1a41f6820d9
Leonardo Bertinatto <lforquim@hotmail.com>
2023-05-24 20:41:51 -0300

    Busca em Profundidade

--- ALESTII/T3-API/src/App.java
+++ ALESTII/T3-API/src/App.java
@@ -1,5 +1,15 @@
+import aula24_grafos_valorado.BuscaEmProfundidade;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+            GrafoValorado g = new GrafoValorado(4);
+            g.adicionarAresta(0, 1, 33);
+            g.adicionarAresta(0, 2, 10);
+            g.adicionarAresta(1, 2, 99);
+            g.adicionarAresta(0, 3, 200);
+            
+        
+
     }
 }
